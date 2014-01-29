e34a64557f5dc4a5d5dea73178112e3ec434e546
Adam Pearce <adam.r.pearce@gmail.com>
2014-01-29 02:08:48 -0500

    adds working testing folder

---
+++ testing/Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      files: ['*.js', '*.html'],
+      options: {
+        nospawn: true,
+        livereload: true,
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
