da3bc22ae18d5d9b0aa88ce1fc2ce337376b6fae
Adam Pearce <adam.r.pearce@gmail.com>
2014-04-29 22:18:40 -0400

    naive bar transition set up

--- source/javascripts/posts/negBarTransition/bar.js
+++ source/javascripts/posts/negBarTransition/bar.js
@@ -25,4 +25,47 @@ svg.selectAll('rect')
   .append('rect')
     .attr('x', function(d, i){ return x(i); })
     .attr('width', x.rangeBand())
-    .attr('height', function(d){ return Math.abs(Math.abs(d) -y(0)) ; })
+    .attr('y', function(d){ return y(d > 0 ? d : 0); })
+    .attr('height', function(d){ return y(0) - y(Math.abs(d)); })
+
+
+d3.select('body').append('h1')
+    .text('Naive Transition')
+    .on('click', function(){
+      svg.selectAll('rect')
+          .data(d3.shuffle(data))
+        .transition().duration(1000)
+          .attr('y', function(d){ return y(d > 0 ? d : 0); })
+          .attr('height', function(d){ return y(0) - y(Math.abs(d)); })
+    })
+
+d3.select('body').append('h1')
+    .text('Tween Transition')
+    .on('click', function(){
+      svg.selectAll('rect')
+          .data(d3.shuffle(data))
+        .transition().duration(1000)
+          .attrTween('y', function(d){
+            var current = d3.select(this)
+          })
+          .attr('y', function(d){ return y(d > 0 ? d : 0); })
+          .attr('height', function(d){ return y(0) - y(Math.abs(d)); })
+    })
+
+function currentBarData(bar){
+  var height = bar.attr('height');
+  var yPos = bar.attr('y');
+  return (yPos < 0 ? -1 : 1)*(y.invert(height) - y(0));
+}
+
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
+
+
+d3.selectAll('h1')
+    .style('cursor', 'pointer')
