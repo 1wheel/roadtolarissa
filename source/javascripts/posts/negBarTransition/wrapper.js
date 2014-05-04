025a9140b4c43287ce89a3a2aea9641bdcdb6b1d
Adam Pearce <adam.r.pearce@gmail.com>
2014-05-04 14:03:43 -0400

    adds color transitions to candleBox

--- source/javascripts/posts/negBarTransition/wrapper.js
+++ source/javascripts/posts/negBarTransition/wrapper.js
@@ -1,6 +1,6 @@
 var interpolationLine = graphs.interpolationLine()
 		.parent(d3.select('#interpolationLine').node())
-		.ease('cubic-in-out')
+		.ease('	1`')
 		.draw();
 
 var tweenCandleBar = graphs.candleBar()
@@ -30,17 +30,20 @@ function updateExtent(extent){
 			.extent(extent)
 			.heightF(compose(valToHeight, i))
 			.yF(compose(valToY, i))
+			.fillF(compose(valToFill, i))
 
 	var hExtent = extent.map(valToHeight)
 	var yExtent = extent.map(valToY)
+	var fExtent = extent.map(valToFill);
 	naiveCandleBar
 			.extent(extent)
 			.heightF(d3.interpolate(hExtent[0], hExtent[1]))
 			.yF(d3.interpolate(yExtent[0], yExtent[1]))
+			.fillF(d3.interpolate(fExtent[0], fExtent[1]))
 
 	function valToHeight(d){ return yScale(0) - yScale(Math.abs(d)); }
 	function valToY(d){ return yScale(d > 0 ? d : 0); }
-
+	function valToFill(d){ return d > 0 ? 'steelblue' : 'red'; }
 
 }
 updateExtent([-15, 10])
