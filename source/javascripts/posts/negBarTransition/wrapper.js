8ad89d5f2177571cf72ce053e94c518a3a2addb0
Adam Pearce <adam.r.pearce@gmail.com>
2014-05-04 12:03:32 -0400

    adds bars

--- source/javascripts/posts/negBarTransition/wrapper.js
+++ source/javascripts/posts/negBarTransition/wrapper.js
@@ -3,22 +3,56 @@ var interpolationLine = graphs.interpolationLine()
 		.ease('cubic-in-out')
 		.draw();
 
-interpolationLine.on('mouseIndex', function(index){
-	console.log(index);
-	interpolationLine.mouse(index);
-})
+var tweenCandleBar = graphs.candleBar()
+		.parent(d3.select('#tweenCandleBar').node())
+		.draw();
+
+
+var naiveCandleBar = graphs.candleBar()
+		.parent(d3.select('#naiveCandleBar').node())
+		.draw();
+
 
 var dispatchingGraphs = [interpolationLine];
-var listeningGraphs = dispatchingGraphs.slice().concat([]);
+var listeningGraphs = dispatchingGraphs.slice().concat([tweenCandleBar, naiveCandleBar]);
 
 dispatchingGraphs.forEach(function(graph){
-	graph.on('mouseIndex', updateHover);
+	graph.on('mouseT', updateHover);
 })
 
-function updateHover(index){
+
+function updateExtent(extent){
+	var yScale = tweenCandleBar.yScale();
+	yScale.domain(extent);
+
+	var i = d3.interpolate(extent[0], extent[1]);
+	tweenCandleBar
+			.extent(extent)
+			.heightF(compose(valToHeight, i))
+			.yF(compose(valToY, i))
+
+	var i = d3.interpolate(extent[0], extent[1]);
+	naiveCandleBar
+			.extent(extent)
+			.heightF(function(t){
+				return yScale(0) - yScale(Math.abs(i(t)));
+			})
+			.yF(function(t){
+				return yScale(i(t) > 0 ? i(t) : 0)
+			})
+
+	function valToHeight(d){ return yScale(0) - yScale(Math.abs(d)); }
+	function valToY(d){ return yScale(d) > 0 ? d : 0; }
+
+
+}
+updateExtent([-15, 10])
+
+
+
+function updateHover(t){
 	listeningGraphs.forEach(function(graph){
-		graph.mouse(index);
+		graph.mouse(t);
 	})
 }
-
-updateHover(0)
+updateHover(0);
