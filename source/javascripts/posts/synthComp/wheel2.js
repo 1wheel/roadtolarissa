f63200d49b5501a0aeb2e022bedae136d4a9a97e
Adam Pearce <adam.r.pearce@gmail.com>
2014-07-19 19:42:48 -0400

    offsets working!

--- source/javascripts/posts/synthComp/wheel2.js
+++ source/javascripts/posts/synthComp/wheel2.js
@@ -1,5 +1,4 @@
-var height = rBlue*radius*2 - 5,
-    numBeatsB = 9;
+var height = rBlue*radius*2 - 5;
 
 //beat number to angle
 var rotationScale = d3.scale.linear()
@@ -25,13 +24,13 @@ var color = d3.scale.ordinal()
 
 //create a g element for each beat
 //rotated so we only have to worry about circular math 
-var beatsM = d3.select('.blue .gearG').selectAll('g')
+var beatsB = d3.select('.blue .gearG').selectAll('g')
     .data(d3.range(numBeatsB)).enter()
   .append('g')
     .attr('transform', function(d){ return 'rotate(' + rotationScale(d) + ')'; })
 
 //add array of notes to each beat
-var notesM = beatsM.selectAll('path')
+beatsB.selectAll('path')
     .data(function(){ return pitches.map(function(d, i){
       return {pitch: d, on: false, i: i}; }); 
     }).enter()
