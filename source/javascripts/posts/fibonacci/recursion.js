2ee5628792660b9bf9b7f5b97dbb73fcf281fee3
Adam Pearce <adam.r.pearce@gmail.com>
2014-08-27 00:24:21 -0400

    strokes working

--- source/javascripts/posts/fibonacci/recursion.js
+++ source/javascripts/posts/fibonacci/recursion.js
@@ -1,7 +1,8 @@
-var height = 500;
-var width = 750;
-var margin = {left: 0, right: 0, top: 15, bottom: 15};
-var topLevel = 10;
+var height = 500,
+    width = 750,
+    margin = {left: 0, right: 0, top: 15, bottom: 15},
+    topLevel = 5,
+    duration = 1000;
 
 var svg = d3.select('#recursion')
   .append('svg')
@@ -16,22 +17,28 @@ var levelToHeight = d3.scale.linear()
 
 var tree = {i: topLevel, left: 0, right: width};
 tree.parent = tree;
-
 addChildren(tree);
+drawCircle(tree);
 
 function addChildren(obj){
   obj.x = (obj.left + obj.right)/2;
   obj.y = levelToHeight(obj.i);
+  obj.childDrawn = false;
+  obj.calculated = false;
 
   if (obj.i === 0 || obj.i === 1){
+    obj.childDrawn = true;
     obj.val = obj.i;
+    obj.children = [];
     return obj;
   }
 
   var mid = (obj.left + obj.right)/2;
+  var cIndex = [obj.i - 1, obj.i - 2];
+  if (Math.random() < .5){ cIndex.reverse(); }
   obj.children = [
-      addChildren({i: obj.i - 1, parent: obj, left: obj.left, right: mid}), 
-      addChildren({i: obj.i - 2, parent: obj, left: mid,      right: obj.right}), 
+      addChildren({i: cIndex[0], parent: obj, leftSide: true,  left: obj.left, right: mid}), 
+      addChildren({i: cIndex[1], parent: obj, leftSide: false, left: mid,       right: obj.right}), 
     ];
 
   obj.val = d3.sum(obj.children, ƒ('val'))
@@ -39,45 +46,59 @@ function addChildren(obj){
 }
 
 function drawCircle(obj){
-  var childDrawn = false;
-  svg.append('circle')
+  obj.circle = svg.append('circle')
       .attr('r', 10)
       .on('mouseover', function(){
-        if (childDrawn) return;
-        if (!obj.children) return;
-        childDrawn = true;
-        drawCircle(obj.children[0])
-        drawCircle(obj.children[1])
-        d3.select(this).style('fill', 'lightgrey')
+        if (!obj.childDrawn){
+          obj.childDrawn = true;
+          drawCircle(obj.children[0])
+          drawCircle(obj.children[1])
+          d3.select(this).style('fill', color(obj))
+        }
+        if (!obj.calculated && obj.children.every(f('calculated'))){
+          obj.calculated = true;
+          d3.select(this).style('fill', color(obj));
+
+          svg.append('path')
+              .attr('d', arc([obj.x, obj.y], [obj.x, obj.y]))
+            .transition().duration(duration)
+              .attr('d', arc([obj.parent.x, obj.parent.y], [obj.x, obj.y]))
+              .each('end', function(){ updateParentState(obj); })
+        }
       })
       .attr('cx', obj.parent.x)
       .attr('cy', obj.parent.y)
       .style('pointer-events', 'none')
-      .style('fill', 'steelblue')
-    .transition()
+      .style('fill', color(obj))
+      .datum(obj);
+
+  obj.circle.transition().duration(duration)
       .attr('cx', obj.x)
       .attr('cy', obj.y)
       .each('end', function(){
         d3.select(this).style('pointer-events', 'all')
       })
 
-  svg.append('line')
-      .attr(
-        { x1: obj.parent.x,
-          y1: obj.parent.y,
-          x2: obj.parent.x,
-          y2: obj.parent.y })
-    .transition()
-      .attr(
-        { x2: obj.x,
-          y2: obj.y })
-      .style({stroke: 'black', "stroke-width": 3});
-
+  svg.append('path')
+      .attr('d', arc([obj.parent.x, obj.parent.y], [obj.parent.x, obj.parent.y]))
+      .style({stroke: 'black', "stroke-width": 2})
+    .transition().duration(duration)
+      .attr('d', arc([obj.x, obj.y], [obj.parent.x, obj.parent.y]))
 
 
 }
 
-function objToX(obj){ return (obj.left + obj.right)/2; }
-function objToY(obj){ return levelToHeight(obj.i); }
+function updateParentState(obj){
+  obj.parent.circle.style('fill', color(obj.parent));
+}
 
-drawCircle(tree);
+function color(obj){
+  return !obj.childDrawn ? 'steelblue' : obj.calculated ? 'white' : obj.children.every(f('calculated')) ? 'red' : 'lightgrey';
+}
+
+function arc(a, b) {
+  var dx = a[0] - b[0],
+      dy = a[1] - b[1],
+      dr = Math.sqrt(dx * dx + dy * dy);
+  return "M" + b[0] + "," + b[1] + "A" + dr + "," + dr + " 0 0,1 " + a[0] + "," + a[1];
+}
