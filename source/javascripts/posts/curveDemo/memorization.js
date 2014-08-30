52c3d8186752a961ba569ec071fa94139aad2135
Adam Pearce <1wheel@gmail.com>
2014-08-29 21:31:20 -0400

    both points connected

--- source/javascripts/posts/curveDemo/memorization.js
+++ source/javascripts/posts/curveDemo/memorization.js
@@ -12,19 +12,23 @@ var svg = d3.select('body')
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 
-var p1 = {x: 30,  y: 30};
-var p2 = {x: 400, y: 100};
-svg.append('circle')
-    .attr({r: 10, cx: p1.x, cy: p1.y})
+var p1 = [30, 30],
+    p2 = [400, 250]
+    c1 = p1.slice(),
+    c2 = p2.slice();
 
-svg.append('circle')
-    .attr({r: 10, cx: p2.x, cy: p2.y})
+c1[0] += 250;
+c2[0] -= 250;
 
-var pathStr = ['M', p1.x, ',', p1.y, ' C', p2.x, ',', p2.y, ' ', 12, ',', 34, ' S400,300 400,200']
-.join('')
+svg.append('circle').attr({r: 10, cx: p1[0], cy: p1[1]})
+svg.append('circle').attr({r: 10, cx: p2[0], cy: p2[1]})
 
-pathStr = ['M', p1.x, ',', '200 C100,100 250,100 250,200 S400,300 400,200'].join('')
-
+pathStr = ['M', p1, 'C', c1, ' ', c2, ' ', p2].join('');
+ 
 svg.append('path')
     .attr('d', pathStr)
 //"M" + p1.x + ' ' p1.y + "C" + p[1] + " " + p[2] + " " + p[3]
+// var pathStr = ['M', p1.x, ',', p1.y, ' C', p2.x, ',', p2.y, ' ', 12, ',', 34, ' S400,300 400,200']
+// .join('')
+
+// pathStr = ['M', p1, 'C100,100 250,100 250,200 S400,300 ', p2].join('');
