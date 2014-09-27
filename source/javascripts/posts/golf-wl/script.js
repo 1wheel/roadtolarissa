220cb7214ac166a78cf2341ef9bdfd0409b69584
Adam Pearce <adam.r.pearce@gmail.com>
2014-09-26 23:59:56 -0400

    building data object

--- source/javascripts/posts/golf-wl/script.js
+++ source/javascripts/posts/golf-wl/script.js
@@ -2,5 +2,13 @@
 // _.flatten(d3.values(data).map(function(d){ return d3.values(d); }))
 
 d3.json('flat-data.json', function(err, data){
-	debugger;
+	var rounds = [];
+	d3.range(0, 18).forEach(function(matchNum){
+		d3.range(-9, 10).forEach(function(spread){
+			if (9 - Math.abs(9 - matchNum) >= spread){
+				rounds.push
+			}
+		})
+	})
+
 })
