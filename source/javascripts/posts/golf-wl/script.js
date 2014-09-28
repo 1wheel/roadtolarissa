d8d32d290e080925997637bc4a4b20a3eceb0804
Adam Pearce <adam.r.pearce@gmail.com>
2014-09-27 21:33:38 -0400

    adds hover

--- source/javascripts/posts/golf-wl/script.js
+++ source/javascripts/posts/golf-wl/script.js
@@ -13,6 +13,9 @@ var y = d3.scale.linear()
 		.domain([-9, 9])
 		.range([height, 0])
 
+var xTick = x(1),
+		yTick = y(8);
+
 var radiusScale = d3.scale.sqrt()
 		.range([0, 10])
 
@@ -28,10 +31,10 @@ var svg = d3.select('#golf-wl')
 
 d3.json('flat-data.json', function(err, matches){
 	var rounds = [];
-	d3.range(0, 19).forEach(function(roundNum){
+	d3.range(0, 19).forEach(function(hold){
 		d3.range(-9, 10).forEach(function(spread){
-			if (9 - Math.abs(9 - roundNum) >= Math.abs(spread)){
-				rounds.push({roundNum: roundNum, spread: spread, count: 0, up: 0, same: 0, down: 0})
+			if (9 - Math.abs(9 - hold) >= Math.abs(spread)){
+				rounds.push({hold: hold, spread: spread, count: 0, up: 0, same: 0, down: 0})
 			}
 		})
 	})
@@ -47,17 +50,18 @@ d3.json('flat-data.json', function(err, matches){
 			match.scores = match.scores.map(function(d){ return -1*d; })
 		}
 
-		match.scores.forEach(function(spread, roundNum){
-			var round = _.findWhere(rounds, {roundNum: roundNum, spread: spread});
+		match.scores.forEach(function(spread, hold){
+			var round = _.findWhere(rounds, {hold: hold, spread: spread});
 			if (!round) return;
 			round.count++;
-			var nextSpread = match.scores[roundNum+1];
+			var nextSpread = match.scores[hold+1];
 			if (isNaN(nextSpread)) return
 			round[nextSpread < spread ? 'down' : nextSpread == spread ? 'same' : 'up']++;
 		})
 	})
 
 	var directions = ['down', 'same', 'up']
+	var color = d3.scale.category10();
 	var maxLineVals = directions.map(function(str){ return d3.max(rounds, ƒ(str)) })
 
 	radiusScale.domain(d3.extent(rounds, f('count')))
@@ -67,7 +71,7 @@ d3.json('flat-data.json', function(err, matches){
 			.data(rounds).enter()
 		.append('g')
 			.attr('transform', function(d){
-				return ['translate(', x(d.roundNum), ',', y(d.spread), ')'].join(''); })
+				return ['translate(', x(d.hold), ',', y(d.spread), ')'].join(''); })
 
 
 	roundGs.selectAll('line')
@@ -77,9 +81,23 @@ d3.json('flat-data.json', function(err, matches){
 				}) })
 			.enter()
 		.append('line')
-			.attr({x2: x(1)})
-			.attr('y2', function(d){ return d.direction*(y(1) - y(0))})
+			.attr({x2: xTick})
+			.attr('y2', function(d){ return d.direction*(-yTick)})
 			.style('stroke-width', _.compose(lineWidthScale, f('count')))
+			.style('stroke', _.compose(color, f('type')))
+
 	roundGs.append('circle')
 			.attr('r', _.compose(radiusScale, f('count')))
+
+	roundGs.append('rect')
+			.attr({x: -xTick/2, y: -yTick/2, width: xTick, height: yTick})
+			.on('mouseover', function(d){
+				d3.select(this).classed('hovered', true)
+			})
+			.on('mouseout', function(d){
+				d3.select(this).classed('hovered', false)
+			})
+			.on('click', function(d){
+
+			})
 })
