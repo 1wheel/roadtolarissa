cbb547bb7a8e28e6887a788625763578cb4ef37a
Adam Pearce <adam.r.pearce@gmail.com>
2014-10-16 00:19:06 -0400

    rects layed correctly

--- source/javascripts/posts/dragonCurve/script.js
+++ source/javascripts/posts/dragonCurve/script.js
@@ -12,14 +12,14 @@ var svg = d3.select('#dragon-curve')
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-function addLine(a, b, ℓ, θ){
+function addLine(a, b, ℓ, θ, out){
   var line = svg.append('path')
       .attr('d', ['M', a, 'L', b].join(''))
 
   var done;
   var rect = svg.append('rect')
       .attr({x: a[0], y: a[1], height: ℓ/sqrt2, width: ℓ/sqrt2})
-      .attr('transform', ['rotate(', θ - 135 ,',', a, ')'].join(''))
+      .attr('transform', ['rotate(', -θ + 45,',', a, ')'].join(''))
       .on('mouseover', function(){
         if (done) return
 
@@ -27,20 +27,20 @@ function addLine(a, b, ℓ, θ){
 
         var θ1 = (θ - 45) % 360
         var b1 = extendLine(a, ℓ1, θ1)
-        addLine(a, b1, ℓ1, θ1)
+        addLine(a, b1, ℓ1, θ1, true)
 
         var θ2 = (θ + 45) % 360
         var a1 = extendLine(b, ℓ1, θ2 - 180)
-        addLine(a1, b, ℓ1, θ2)
+        addLine(a1, b, ℓ1, θ2, false)
 
         line.style('opacity', .2)
-        rect.style('opacity', .05)
+        rect.style('opacity', .05).remove()
         done = true
       })
 }
 
 var length = width
-addLine([0, height/2], [length, height/2], length, 90)
+addLine([0, height/2], [length, height/2], length, 90, true)
 
 
 function extendLine(a, ℓ, θ){
