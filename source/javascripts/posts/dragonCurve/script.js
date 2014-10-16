7b8ad6e29f015a8fb19220f13cf27be9b5b21fbb
Adam Pearce <adam.r.pearce@gmail.com>
2014-10-16 00:36:29 -0400

    adds reset button

--- source/javascripts/posts/dragonCurve/script.js
+++ source/javascripts/posts/dragonCurve/script.js
@@ -3,7 +3,7 @@ var π = Math.PI
 
 var width = 400,
     height = 400,
-    margin = {left: 200, right: 200, top: 200, bottom: 200}
+    margin = {left: 150, right: 80, top: 0, bottom: 100}
 
 var svg = d3.select('#dragon-curve')
     .append('svg')
@@ -25,13 +25,13 @@ function addLine(a, b, ℓ, θ, out){
 
         var ℓ1 = ℓ/2*sqrt2
 
-        var θ1 = (θ - 45) % 360
+        var θ1 = (θ - 45*out) % 360
         var b1 = extendLine(a, ℓ1, θ1)
-        addLine(a, b1, ℓ1, θ1, true)
+        addLine(a, b1, ℓ1, θ1, 1)
 
-        var θ2 = (θ + 45) % 360
+        var θ2 = (θ + 45*out) % 360
         var a1 = extendLine(b, ℓ1, θ2 - 180)
-        addLine(a1, b, ℓ1, θ2, false)
+        addLine(a1, b, ℓ1, θ2, -1)
 
         line.style('opacity', .2)
         rect.style('opacity', .05).remove()
@@ -46,3 +46,14 @@ addLine([0, height/2], [length, height/2], length, 90, true)
 function extendLine(a, ℓ, θ){
   return [a[0] + ℓ*Math.sin(θ*π/180), a[1] + ℓ*Math.cos(θ*π/180)]
 }
+
+d3.select('#step').on('click', function(){
+  d3.selectAll('rect')
+      .each(function(){ d3.select(this).on('mouseover')() })
+})
+
+d3.select('#reset').on('click', function(){
+  svg.selectAll('*').remove()
+  addLine([0, height/2], [length, height/2], length, 90, true)
+})
+
