9f18c47db27a3dbdc5899586e28956e782a877d0
Adam Pearce <adam.r.pearce@gmail.com>
2014-10-17 22:38:31 -0400

    derives length

--- source/javascripts/posts/dragonCurve/script.js
+++ source/javascripts/posts/dragonCurve/script.js
@@ -12,9 +12,10 @@ var svg = d3.select('#dragon-curve')
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-function addLine(a, b, c, ℓ, θ, out){
+function addLine(a, b, m, θ, out){
+  var ℓ = length(a, b)
   var line = svg.append('path')
-    //   .attr('d', ['M', a, 'L', c].join(''))
+    //   .attr('d', ['M', a, 'L', m].join(''))
     // .transition().duration(1000)
       .attr('d', ['M', a, 'L', b].join(''))
       // .each('end', function(){
@@ -33,11 +34,11 @@ function addLine(a, b, c, ℓ, θ, out){
 
             var θ1 = (θ - 45*out) % 360
             var b1 = extendLine(a, ℓ1, θ1)
-            addLine(a, b1, m1, ℓ1, θ1, 1)
+            addLine(a, b1, m1, θ1, 1)
 
             var θ2 = (θ + 45*out) % 360
             var a2 = extendLine(b, ℓ1, θ2 - 180)
-            addLine(a2, b, m1, ℓ1, θ2, -1)
+            addLine(a2, b, m1, θ2, -1)
 
             line.style('opacity', .2)
             rect.style('opacity', .05).remove()
@@ -46,12 +47,17 @@ function addLine(a, b, c, ℓ, θ, out){
 
 }
 
-var length = width
-addLine([0, height/2], [length, height/2], [0, height/2], length, 90, 1)
+addLine([0, height/2], [width, height/2], [0, height/2], 90, 1)
 
 
 function extendLine(a, ℓ, θ){
   return [a[0] + ℓ*Math.sin(θ*π/180), a[1] + ℓ*Math.cos(θ*π/180)]
+}
+
+function length(a, b){
+  var x2 = Math.pow(a[0] - b[0], 2)
+  var y2 = Math.pow(a[1] - b[1], 2)
+  return Math.sqrt(x2 + y2)
 }
 
 function midPoint(a, b){
