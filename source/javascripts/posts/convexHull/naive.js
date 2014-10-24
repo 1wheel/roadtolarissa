c7275f45b4a2e97623b85952233ca0c1db91c975
Adam Pearce <adam.r.pearce@gmail.com>
2014-10-23 23:07:32 -0400

    better colors

--- source/javascripts/posts/convexHull/naive.js
+++ source/javascripts/posts/convexHull/naive.js
@@ -1,3 +1,8 @@
+var green = '#01863e',
+		blue = '#1c4695',
+		red = '#ec3221'
+
+
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 750 - margin.left - margin.right
     height = 500 - margin.top - margin.bottom
@@ -15,7 +20,7 @@ var svg = d3.select('#naive')
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-svg.append('path')
+var pairLine = svg.append('path')
     .classed('pairLine', true)
     .attr('marker-end', 'url(#head)')
 var pairLineG = svg.append('g')
@@ -29,7 +34,7 @@ svg.append('defs').append('marker')
 circles = svg.selectAll('circle')
     .data(points).enter()
   .append('circle').classed('point', true)
-    .attr('r', 5)
+    .attr('r', 3)
     .attr('cx', f('x'))
     .attr('cy', f('y'))
     .each(function(d){ d.circle = d3.select(this) })
@@ -47,20 +52,29 @@ d3.range(numPoints).forEach(function(i){
 trialLineG.selectAll('trialLine')
     .data(pairs).enter()
   .append('path').classed('trialLine', true)
-    .on('mouseover', trialMouseOver)
-    .style({stroke: 'darkgrey', 'stroke-width': 4})
+    .on('mouseover', function(d){
+    	if (this.__transition__) return
+    	drawPairLine(d)
+    	d3.select(this).attr({class: 'pairLine'}).style('stroke-width', 4)
+    })
+    .on('mouseout', function(d){
+    	if (this.__transition__) return
+			d3.select(this).transition().duration(1000)
+					.attr('d', ['M', d.b.x, ',', d.b.y, ' L', d.b.x, ',', d.b.y].join(''))
+					.remove()
+    })
+    .style({opacity: '1', 'stroke-width': 4})
     .attr('d', function(d){
       return ['M', d.a.p, 'L', d.a.p].join('') })
-  .transition().delay(function(d){ return d.i*0000 }).duration(800)
+  .transition().delay(function(d){ return d.i*800 }).duration(800)
+  	.each('start', function(d){
+  		d.a.circle.transition().attr('r', 10)
+  	})
     .attr('d', function(d){
       return ['M', d.a.p, 'L', d.b.p].join('') })
   .transition()
-    .style('stroke-width', 2)
+    .style({'stroke-width': 1, opacity: .8})
 
-function trialMouseOver(pair){
-  drawPairLine(pair)
-  d3.select(this).remove()
-}
 
 var curPairIndex = 0;
 function iteratePair(){
@@ -78,13 +92,17 @@ function drawPairLine(pair){
   var otherCircles = circles
       .classed('left', false)
       .classed('right', false)
+    .transition()
       .attr('r', 5)
       .filter(function(d, i){ return i != a.i && i != b.i })
 
-  a .circle.attr('r', 10).classed('left',  true)
-  b.circle.attr('r', 10).classed('right', true)
-  d3.select('.pairLine')
-      .attr('d', ['M', a.x, ',', a.y, ' L', b.x, ',', b.y].join(''))
+  a.circle.classed('left',  true)
+  	.transition()
+		  .attr('r', 10)
+  b.circle.classed('right', true)
+  	.transition()
+  		.attr('r', 10)
+  //pairLine.attr('d', ['M', a.x, ',', a.y, ' L', b.x, ',', b.y].join(''))
 
   var m = (a.y - b.y)/(a.x - b.x)
   var B = a.y - m*a.x
@@ -94,7 +112,7 @@ function drawPairLine(pair){
   otherCircles.style('fill', function(d){
     var isLeft = dir ^ (d.x*m + B > d.y)
     allLeft = allLeft && isLeft
-    return isLeft ? 'yellow' : 'blue'
+    return isLeft ? blue : red
   })
   if (allLeft){
     pairLineG.append('path')
