35987fdf2519cae916a5b97a09616e76968e08eb
Adam Pearce <adam.r.pearce@gmail.com>
2014-10-29 00:23:46 -0400

    calcs above correctly

--- source/javascripts/posts/convexHull/nlogn.js
+++ source/javascripts/posts/convexHull/nlogn.js
@@ -29,39 +29,43 @@ function drawNlogN(){
   var curI = 1
 
   function iteratePoint(){
-    return
     curI++
     if (curI > points.length  - 1) return
     topPoints.push(points[curI])
     var b = points[curI - 1]
 
-    ang = calcAngle(points[curI - 2], b, points[curI])*180/Math.PI
+    var keep = !lessThan180(points[curI - 2], b, points[curI])
 
-    b.circle.attr('r', 10)
-    b.circle.style('fill', ang < 45 ? 'green' : 'red')
-    //console.log(ang)
+    b.circle
+    b.circle.transition()
+        .style('fill', keep ? 'green' : 'steelblue')
+        .style('fill-opacity', .7)
+        .attr('r', 10)
+
   }
 
   setInterval(iteratePoint, 500)
 
-  svg.append('rect')
-      .attr({width: width, height: height})
-      .on('mousemove', function(){
-        var p3 = d3.mouse(this)
-        var p3 = {x: p3[0], y: p3[1]}
-        mPath.attr('d', ['M', p2.x, ',', p2.y, 'L', p3.x, ',', p3.y].join(''))
-        //console.log(calcAngle(p1, p2, p3))
-        ang = isLessThan180(p1, p2, p3)
-        aText.text(ang)
-      })
-  var p1 = {x: 200, y: 200}
-  var p2 = {x: 300, y: 200}
-  svg.append('path').classed('mline', true)
-      .attr('d', ['M', p1.x, ',', p1.y, 'L', p2.x, ',', p2.y].join(''))
+  // svg.append('rect')
+  //     .attr({width: width, height: height})
+  //     .on('mousemove', function(){
+  //       var p3 = d3.mouse(this)
+  //       var p3 = {x: p3[0], y: p3[1]}
+  //       mPath.attr('d', ['M', p2.x, ',', p2.y, 'L', p3.x, ',', p3.y].join(''))
+  //       //console.log(calcAngle(p1, p2, p3))
+  //       ang = isLessThan180(p1, p2, p3)
+  //       aText.text(ang)
+  //     })
+  // var p1 = {x: 200, y: 200}
+  // var p2 = {x: 300, y: 200}
+  // svg.append('path').classed('mline', true)
+  //     .attr('d', ['M', p1.x, ',', p1.y, 'L', p2.x, ',', p2.y].join(''))
 
-  var mPath = svg.append('path').classed('mline', true)
-  var aText = svg.append('text').style('fill', 'white')
-      .attr({dy: '3em', dx: '3em'})
+  // var mPath = svg.append('path').classed('mline', true)
+  // var aText = svg.append('text').style('fill', 'white')
+  //     .attr({dy: '3em', dx: '3em'})
+
+
   svg.append('text').classed('reset-button', true)
       .attr({dy: '1em', dx: '.2em'})
       .on('click', drawNlogN)
@@ -92,7 +96,7 @@ function dist(a, b){
   return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 }
 
-function isLessThan180(a, b, c){
+function lessThan180(a, b, c){
   var v1 = [b.x - a.x, b.y - a.y]
   var v2 = [c.x - b.x, c.y - b.y]
   
