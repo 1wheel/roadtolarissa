20e36b96fcb328b81cf9999fa4c297e2b361b663
Adam Pearce <adam.r.pearce@gmail.com>
2014-12-20 20:26:28 -0500

    more mouse over hn

--- source/javascripts/posts/convexHull/hn.js
+++ source/javascripts/posts/convexHull/hn.js
@@ -17,10 +17,8 @@ function drawHN(){
       .attr('cy', f('y'))
       .each(function(d){ d.circle = d3.select(this) })
 
-  var mLine = svg.append('line').classed('mline', true)
-
   var hullPoints = []
-  var curPoint,prevPoint, maxAngle;
+  var curPoint,prevPoint, maxAngle, updating;
   function updateCurPoint(cur, prev){
     if (cur.outline) return
     curPoint = cur
@@ -36,8 +34,6 @@ function drawHN(){
       d.circle.classed('next-point', d.active)
     })    
 
-    mLine.attr({x1: curPoint.x, y1: curPoint.y})
-
     svg.append('path').classed('outline', true)
         .datum({prev: prevPoint, cur: curPoint})
         .attr('d', ['M', prevPoint.x, prevPoint.y, 'L', curPoint.p].join(' '))
@@ -46,6 +42,18 @@ function drawHN(){
       d3.select('.outline').remove()  
     }
 
+    updating = true
+    circles.transition()
+        .attr('r', 10)
+        .style('fill', 'black')
+
+    d3.selectAll('.possible-max')
+      .transition().duration(1000)
+        .attr('x1', ƒ('x'))
+        .attr('y1', ƒ('y'))
+        .remove()
+
+    window.setTimeout(function(){ updating = false }, 1000)
   }
   updateCurPoint(points[0], {x: points[0].x, y: height})
 
@@ -53,37 +61,39 @@ function drawHN(){
       .style('fill-opacity', 0)
       .attr({width: width, height: height})
       .on('mousemove', function(){
+        if (updating) return
+
         var pos = d3.mouse(this)
-        mLine.attr({x2: pos[0], y2: pos[1]})
 
         var angle = calcAngle(prevPoint, curPoint, {x: pos[0], y: pos[1]})
         var oldMaxAngle = maxAngle
 
         points.filter(ƒ('active')).forEach(function(d){
           if (Math.abs(angle - d.angle) < 1){
+            if (angle > maxAngle) maxAngle = d.angle
+
             d.active = false
             d.circle.classed('next-point', false)
-            if (angle > maxAngle){
-              maxAngle = d.angle
-              svg.append('line').datum(d).attr('class', 'possible-max growing')
-                  .attr({x1: curPoint.x, y1: curPoint.y, x2: curPoint.x, y2: curPoint.y})
-                .transition('drawInit').duration(1500)
-                  .attr({x2: d.x, y2: d.y})
-            }
+            svg.append('line').datum(d).attr('class', 'possible-max max-angle')
+                //.classed('max-angle', maxAngle == d.angle)
+                .attr({x1: curPoint.x, y1: curPoint.y, x2: curPoint.x, y2: curPoint.y})
+              .transition('drawInit').duration(1000)
+                .attr({x2: d.x, y2: d.y})
+
           }
         })
 
-        if (oldMaxAngle !=  maxAngle){
-          svg.selectAll('.growing').filter(function(d){ return d.angle != maxAngle })
-              .classed('growing', false)
-            .transition('shrinking').duration(1500)
-              .attr('x1', ƒ('x'))
-              .attr('y1', ƒ('y'))
-              .remove()
-        }
+        svg.selectAll('.max-angle').filter(function(d){ return d.angle != maxAngle })
+            .classed('max-angle', false)
+          .transition('shrinking').delay(800).duration(0)
+            .each('end', function(d){
+              d.circle.transition()
+                  .style('fill', 'grey')
+                  .attr('r', 3)
+            })
 
         if (!points.filter(f('active')).length){
-          updateCurPoint(svg.select('.growing').datum(), curPoint)
+          updateCurPoint(_.findWhere(points, {angle: maxAngle}), curPoint)
         }
       })
 
@@ -92,3 +102,21 @@ function drawHN(){
 
 }
 drawHN()
+
+//paste in the console to swap out the d3.scale.category20() pallet
+var oldColors = oldColors ? oldColors : d3.scale.category20().range()
+var newColors = oldColors.map(function(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16) })
+oldColors.forEach(function(str, i){
+  //update inline color style
+  d3.selectAll('[style*="' + str + '"]')
+      .style('fill', newColors[i])
+      .style('background-color', newColors[i])
+
+  //update fill attr 
+  d3.selectAll('[fill*="' + str + '"]')
+      .attr('fill', newColors[i])
+})
+
+//save new pallet - press up arrow and enter to run again
+oldColors = newColors
