495b8008c69f0cce065948ceceadfeab6b6ad103
Adam Pearce <adam.r.pearce@gmail.com>
2014-12-22 03:55:37 -0500

    cleaning up

--- source/javascripts/posts/convexHull/hn.js
+++ source/javascripts/posts/convexHull/hn.js
@@ -14,7 +14,7 @@ function drawHN(){
   var circles = svg.append('g').selectAll('circle')
       .data(points).enter()
     .append('circle').classed('point', true)
-      .attr('r', 5)
+      .attr('r', 8)
       .attr('cx', f('x'))
       .attr('cy', f('y'))
       .each(function(d){ d.circle = d3.select(this) })
@@ -34,35 +34,36 @@ function drawHN(){
     hullPoints.push(curPoint)
     curPoint.outline = true
 
-    lineG.append('line').classed('outline', true)
-        .datum({prev: prevPoint, cur: curPoint})
-        .attr({x1: prevPoint.x, x2: prevPoint.x, y1: prevPoint.y, y2: prevPoint.y})
-      .transition().duration(700).delay(3000)
-        .attr({x2: curPoint.x, y2: curPoint.y})
-
-
-    updating = true
-    circles.transition().duration(1000).delay(3500)
-        .attr('r', 8)
-
-    d3.selectAll('.possible-max')
-      .transition().duration(1000).delay(function(d){ return 200 + points.length*50 -d.index*50 })
-        .attr('x1', ƒ('x')) 
-        .attr('y1', ƒ('y'))
-        .remove()
-
     points.forEach(function(d){
       d.angle = calcAngle(prevPoint, curPoint, d)
       d.active = d != curPoint
       d.circle.classed('hoverable', d.active)
     })    
 
+    updating = true
+
+    d3.selectAll('.possible-max')
+      .transition().duration(1000).delay(function(d){ return 200 + points.length*20 - d.index*20 })
+        .attr('x1', ƒ('x')) 
+        .attr('y1', ƒ('y'))
+        .remove()
+
+    lineG.append('line').classed('outline', true)
+        .datum({prev: prevPoint, cur: curPoint})
+        .attr({x1: prevPoint.x, x2: prevPoint.x, y1: prevPoint.y, y2: prevPoint.y})
+      .transition().duration(700).delay(points.length*20+1000)
+        .attr({x2: curPoint.x, y2: curPoint.y})
+
+    circles.transition().duration(1000).delay(points.length*20+1500)
+        .attr('r', 8)
+
     points = _.sortBy(points, f('angle')).reverse()
     points.forEach(function(d, i){ d.index = i })
 
-    window.setTimeout(function(){ updating = false }, 3500)
+    window.setTimeout(function(){ updating = false }, points.length*20+1500)
   }
   updateCurPoint(points[0], {x: points[0].x, y: points[0].y + 1})
+  updating = false
 
   svg.append('rect')
       .style('fill-opacity', 0)
@@ -99,25 +100,9 @@ function drawHN(){
       })
 
 
-
-
+  svg.append('text').classed('reset-button', true)
+      .attr({dy: '1em', dx: '.2em'})
+      .on('click', drawHN)
+      .text('↻')
 }
 drawHN()
-
-//paste in the console to swap out the d3.scale.category20() pallet
-var oldColors = oldColors ? oldColors : d3.scale.category20().range()
-var newColors = oldColors.map(function(){
-  return '#'+Math.floor(Math.random()*16777215).toString(16) })
-oldColors.forEach(function(str, i){
-  //update inline color style
-  d3.selectAll('[style*="' + str + '"]')
-      .style('fill', newColors[i])
-      .style('background-color', newColors[i])
-
-  //update fill attr 
-  d3.selectAll('[fill*="' + str + '"]')
-      .attr('fill', newColors[i])
-})
-
-//save new pallet - press up arrow and enter to run again
-oldColors = newColors
