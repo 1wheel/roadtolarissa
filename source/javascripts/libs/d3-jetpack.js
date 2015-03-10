44c7efc683fddb903915f59bc2b7a629b58edb20
Adam Pearce <1wheel@gmail.com>
2015-03-09 23:59:14 -0400

    jetpack pudate

--- source/javascripts/libs/d3-jetpack.js
+++ source/javascripts/libs/d3-jetpack.js
@@ -1,3 +1,5 @@
+function ƒ(str){ return function(d){ return typeof(str) == 'undefined' ? d : d[str] } } 
+
 (function() {
         
     function jetpack(d3) {
@@ -46,6 +48,27 @@
             });
             return n.attr ? s.attr(n.attr) : s;
         };
+
+        d3.selection.prototype.styleC = function(name){
+            return this.style(name, d3.compose.apply(null, [].slice.call(arguments, 1)))
+        }
+        d3.selection.prototype.attrC = function(name){
+            return this.attr(name, d3.compose.apply(null, [].slice.call(arguments, 1)))
+        }
+        d3.transition.prototype.styleC = function(name){
+            return this.style(name, d3.compose.apply(null, [].slice.call(arguments, 1)))
+        }
+        d3.transition.prototype.attrC = function(name){
+            return this.attr(name, d3.compose.apply(null, [].slice.call(arguments, 1)))
+        }
+
+        d3.selection.prototype.appendData = function(name, data){
+            return this.selectAll('#zzzzzz')
+                    .data(data)
+                    .enter()
+                    .append(name)
+        }
+
 
         var d3_parse_attributes_regex = /([\.#])/g;
 
@@ -113,6 +136,94 @@
                 return b[key] < a[key] ? -1 : b[key] > a[key] ? 1 : b[key] >= a[key] ? 0 : NaN;
             };
         };
+
+        d3.conventions = function(c){
+          c = c || {}
+
+          c.width  = c.width  || 900
+          c.height = c.height || 500
+
+          c.margin = c.margin || {top: 20, right: 20, bottom: 20, left: 25}
+
+          c.parentSel = c.parentSel || d3.select('body')
+
+          c.svg = c.svg || c.parentSel.append("svg")
+              .attr("width", c.width + c.margin.left + c.margin.right)
+              .attr("height", c.height + c.margin.top + c.margin.bottom)
+            .append("g")
+              .attr("transform", "translate(" + c.margin.left + "," + c.margin.top + ")")
+
+          c.color   = c.color   || d3.scale.category10()
+          c.x       = c.x       || d3.scale.linear().range([0, c.width])
+          c.y       = c.y       || d3.scale.linear().range([c.height, 0])
+          c.rScale  = c.rScale  || d3.scale.sqrt().range([5, 20])
+          c.line    = c.line    || d3.svg.line()
+
+
+          c.xAxis = c.xAxis || d3.svg.axis().scale(c.x).orient("bottom");
+          c.yAxis = c.yAxis || d3.svg.axis().scale(c.y).orient("left")
+
+
+          c.drawAxis = function(){
+            c.svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + c.height + ")")
+                .call(c.xAxis);
+
+            c.svg.append("g")
+                .attr("class", "y axis")
+                .call(c.yAxis);
+          }
+          
+          return c
+        }
+
+        d3.attachTooltip = function(sel, fieldFns){
+          sel 
+              .on('mouseover', ttDisplay)
+              .on('mousemove', ttMove)
+              .on('mouseout',  ttHide)
+
+          function ttDisplay(d){
+            d3.select('.tooltip')
+                .classed('tooltip-hidden', false)
+                .html('')
+              .appendData('div', fieldFns || d3.keys(d).map(function(str){ return function(d){ return str + ': ' + d[str] } }))
+                .text(function(fn){ return fn(d) })
+
+            d3.select(this).classed('tooltipped', true)
+          }
+
+          function ttMove(d){
+            var tt = d3.select('.tooltip')
+            if (!tt.size()) return
+            var e = d3.event,
+              x = e.clientX,
+              y = e.clientY,
+              doctop = (window.scrollY)? window.scrollY : (document.documentElement && document.documentElement.scrollTop)? document.documentElement.scrollTop : document.body.scrollTop;
+              n = tt.node(),
+              nBB = n.getBoundingClientRect()
+
+            tt.style('top', (y+doctop-nBB.height-18)+"px");
+            tt.style('left', Math.min(Math.max(0, (x-nBB.width/2)), window.innerWidth - nBB.width)+"px");
+          }
+
+          function ttHide(d){
+            d3.select('.tooltip').classed('tooltip-hidden', true);
+
+            d3.selectAll('.tooltipped').classed('tooltipped', false)
+          }
+        }
+
+        d3.compose = function(){
+          var functions = arguments 
+          return function(d){
+            var i = functions.length
+            while(i--) d = functions[i].call(this, d)
+            return d
+          }
+        }
+
     }
 
     if (typeof d3 === 'object' && d3.version) jetpack(d3);
