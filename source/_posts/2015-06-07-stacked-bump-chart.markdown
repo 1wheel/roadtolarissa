7e028d57ea629860f4f3b496cfaec19bf1b34d42
Adam Pearce <1wheel@gmail.com>
2015-06-07 20:25:17 -0400

    color fiddles

--- source/_posts/2015-06-07-stacked-bump-chart.markdown
+++ source/_posts/2015-06-07-stacked-bump-chart.markdown
@@ -1,7 +1,6 @@
 ---
 layout: post
 title: "Stacked Bump Charts"
-date: 2015-06-07 00:10:29 -0400
 comments: true
 categories: 
 ---
@@ -12,18 +11,39 @@ Alicia Parlapiano published an excellent [series of stacked bump charts](http://
 
 Her graphic does a great job showing both the size of the field and the duration of the primary campaigns, while previous pieces by the [Economist](http://www.economist.com/blogs/graphicdetail/2015/04/us-presidential-candidate-announcements) and [Bloomberg](http://www.bloomberg.com/politics/articles/2014-11-25/when-do-presidential-candidates-announce) focused on how the duration of campaigns has changed over time.
 
-I've thinking about using a similar stacked bump chart to improve my (still very rough) (visualization)[http://bl.ocks.org/1wheel/cbd9053de9bb39231924] of a line intersection algorithm. To get a feel for how to make one, I've recreated Alicia's chart with Basketball Reference's list of [most accomplished players](http://www.basketball-reference.com/leaders/hof_prob.html). The rest of the this post will show how to make your own bump chart with D3. 
+I've thinking about using a similar stacked bump chart to improve my (still very rough) [visualization](http://bl.ocks.org/1wheel/cbd9053de9bb39231924) of a line intersection algorithm. To get a feel for how to make one, I've recreated Alicia's chart with Basketball Reference's list of [most accomplished players](http://www.basketball-reference.com/leaders/hof_prob.html). The rest of the this post will show how to make your own bump chart with D3. 
 
 First, the data needs to be in the right format. Since we're just showing when each player enters and exits the league (and ignoring complications like Jordan's MLG career), lets start with an array of objects with exactly that info:
 
 ```javascript
 var players = [
-	{name: 'Kareem', 		start: 1970,	stop: 1989},
-	{name: 'Jordan', 		start: 1985,	stop: 2003},
-	{name: 'Russell', 	start: 1957,	stop: 1969},
-	{name: 'Wilt', 			start: 1959,	stop: 1969},
-	{name: 'Kobe', 			start: 1997,	stop: 2015},
-	{name: 'Duncan',    start: 1998,	stop: 2015},
+	{name: 'Kareem',    start: 1970,  stop: 1989},
+  {name: 'Jordan',    start: 1985,  stop: 2003},
+  {name: 'Russell',   start: 1957,  stop: 1969},
+  {name: 'Wilt',      start: 1959,  stop: 1969},
+  {name: 'Kobe',      start: 1997,  stop: 2015},
+  {name: 'Duncan',    start: 1998,  stop: 2015},
 	...
 ]
 ```
+
+
+<div id='line'></div>
+
+Words, words words
+
+<div id='bump'></div>
+
+
+
+
+<link rel="stylesheet" type="text/css" href="/javascripts/posts/stackedBump/style.css">
+
+
+<script src="/javascripts/libs/d3.4.11.js" type="text/javascript"></script>
+<script src="/javascripts/libs/lodash.js" type="text/javascript"></script>
+<script src="/javascripts/libs/d3-jetpack-v1.js" type="text/javascript"></script>
+<script src="/javascripts/libs/d3-starterkit-v0.js" type="text/javascript"></script>
+
+
+<script src="/javascripts/posts/stackedBump/script.js"></script>
--- source/javascripts/posts/stackedBump/script.js
+++ source/javascripts/posts/stackedBump/script.js
@@ -1,63 +1,63 @@
 var players = [
-	{name: 'Kareem', 		start: 1970,	stop: 1989},
-	{name: 'Jordan', 		start: 1985,	stop: 2003},
-	{name: 'Russell', 	start: 1957,	stop: 1969},
-	{name: 'Wilt', 			start: 1959,	stop: 1969},
-	{name: 'Kobe', 			start: 1997,	stop: 2015},
-	{name: 'Duncan',    start: 1998,	stop: 2015},
-	{name: 'Shaq',	    start: 1993,	stop: 2011},
-	{name: 'Cousy',	    start: 1951,	stop: 1963},
-	{name: 'Garnett',	  start: 1995,	stop: 2015},
-	{name: 'West',	    start: 1961,	stop: 1974},
-	{name: 'Magic',	    start: 1980,	stop: 1996},
-	{name: 'K. Malone',	start: 1986,	stop: 2004},
-	{name: 'LeBron',    start: 2004,	stop: 2015},
-	{name: 'Olajuwon',  start: 1985,	stop: 2002},
-	{name: 'Bird',  		start: 1980,	stop: 1982},
-	{name: 'Hayes',  		start: 1969,	stop: 1984},
-	{name: 'Robertson', start: 1975,	stop: 1995},
-	{name: 'M. Malone', start: 1975,	stop: 1995},
-	{name: 'Schayes',   start: 1950,	stop: 1964},
-	{name: 'Dirk',   		start: 1999,	stop: 2015},
-	{name: 'Stockton',  start: 1961,	stop: 1974},
-	{name: 'Pettit',    start: 1955,	stop: 1965},
-	{name: 'Isiah',     start: 1982,	stop: 1994},
+  {name: 'Kareem',    start: 1970,  stop: 1989},
+  {name: 'Jordan',    start: 1985,  stop: 2003},
+  {name: 'Russell',   start: 1957,  stop: 1969},
+  {name: 'Wilt',      start: 1959,  stop: 1969},
+  {name: 'Kobe',      start: 1997,  stop: 2015},
+  {name: 'Duncan',    start: 1998,  stop: 2015},
+  {name: 'Shaq',      start: 1993,  stop: 2011},
+  {name: 'Cousy',     start: 1951,  stop: 1963},
+  {name: 'Garnett',   start: 1995,  stop: 2015},
+  {name: 'West',      start: 1961,  stop: 1974},
+  {name: 'Magic',     start: 1980,  stop: 1996},
+  {name: 'K. Malone', start: 1986,  stop: 2004},
+  {name: 'LeBron',    start: 2004,  stop: 2015},
+  {name: 'Olajuwon',  start: 1985,  stop: 2002},
+  {name: 'Bird',      start: 1980,  stop: 1992},
+  {name: 'Hayes',     start: 1969,  stop: 1984},
+  {name: 'Robertson', start: 1975,  stop: 1995},
+  {name: 'M. Malone', start: 1975,  stop: 1995},
+  {name: 'Schayes',   start: 1950,  stop: 1964},
+  {name: 'Dirk',      start: 1999,  stop: 2015},
+  {name: 'Stockton',  start: 1961,  stop: 1974},
+  {name: 'Pettit',    start: 1955,  stop: 1965},
+  {name: 'Isiah',     start: 1982,  stop: 1994},
 ]
 
-players = _.sortBy(players, 'start').reverse()
+// players = _.sortBy(players, 'start').reverse()
 
 var c = d3.conventions({height: 250, parentSel: d3.select('#lines')})
 c.x.domain([1950, 2015])
 c.y.domain([0, players.length])
 
 c.yAxis
-		.tickFormat(function(d){ return players[d].name })
-		.tickValues(d3.range(players.length))
+    .tickFormat(function(d){ return players[d].name })
+    .tickValues(d3.range(players.length))
 
 c.drawAxis()
 
 c.svg.dataAppend(players, 'g.player')
-		.translate(function(d, i){ return [0, c.y(i)] })
-	.append('line')
-		.attr('x1', ƒ('start', c.x))
-		.attr('x2', ƒ('stop' , c.x))
-		.style({stroke: 'steelblue', 'stroke-width': 4})
+    .translate(function(d, i){ return [0, c.y(i)] })
+  .append('line')
+    .attr('x1', ƒ('start', c.x))
+    .attr('x2', ƒ('stop' , c.x))
+    .style({stroke: 'steelblue', 'stroke-width': 4})
 
 
-
+players = _.sortBy(players, 'start')
 players.forEach(function(d){ d.years = [] })
 
 
 d3.range(1950, 2016).forEach(function(year){
-	var currentHeight = 0
-	players.forEach(function(d){
-		if (d.start <= year && year <= d.stop){
-			d.years.push({year: year, height: ++currentHeight})
-			if (d.stop  == year) d.stopHeight  = currentHeight
-			if (d.start == year) d.startHeight = currentHeight
+  var currentHeight = 0
+  players.forEach(function(d){
+    if (d.start <= year && year <= d.stop){
+      d.years.push({year: year, height: ++currentHeight})
+      if (d.stop  == year) d.stopHeight  = currentHeight
+      if (d.start == year) d.startHeight = currentHeight
 
-		}
-	})
+    }
+  })
 })
 
 
@@ -71,23 +71,23 @@ c.yAxis.ticks(2)
 c.drawAxis()
 
 var line = d3.svg.line()
-		.x(ƒ('year', c.x))
-		.y(ƒ('height', c.y))
+    .x(ƒ('year', c.x))
+    .y(ƒ('height', c.y))
 
 c.svg.dataAppend(players, 'path.player')
-		.attr('d', ƒ('years', line))
+    .attr('d', ƒ('years', line))
 
 c.svg.dataAppend(players, 'circle.start')
-		.attr({cx: ƒ('stop', c.x), cy: ƒ('stopHeight', c.y)})
-		.attr({r: 3, fill: 'white'})
+    .attr({cx: ƒ('stop', c.x), cy: ƒ('stopHeight', c.y)})
+    .attr({r: 3, fill: 'white'})
 
 c.svg.dataAppend(players, 'circle.stop')
-		.attr({cx: ƒ('start', c.x), cy: ƒ('startHeight', c.y)})
-		.attr({r: 3, fill: 'steelblue'})
+    .attr({cx: ƒ('start', c.x), cy: ƒ('startHeight', c.y)})
+    .attr({r: 3, fill: 'steelblue'})
 
 c.svg.dataAppend(players, 'text.name')
-		.attr({x: ƒ('start', c.x), y: ƒ('startHeight', c.y)})
-		.text(ƒ('name'))
-		.attr({'text-anchor': 'end', 'dy': '.33em', 'dx': '-.5em'})
+    .attr({x: ƒ('start', c.x), y: ƒ('startHeight', c.y)})
+    .text(ƒ('name'))
+    .attr({'text-anchor': 'end', 'dy': '.33em', 'dx': '-.5em'})
 
 
