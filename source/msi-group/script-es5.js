a25ccc49d5d19a73488c8b1ad2e4a6e32f5d0f5c
Adam Pearce <1wheel@gmail.com>
2017-05-14 17:25:43 -0400

    we udpate

--- source/msi-group/script-es5.js
+++ source/msi-group/script-es5.js
@@ -70,7 +70,7 @@ var teamSel = treamGroupSel.appendMany(ƒ(), 'g.team-dot')
 teamSel.append('rect')
   .at({width: width, height: height, fill: ƒ(color)})
 teamSel.st({opacity: function (d) {
-  if (d.s.str[0] == 2 || d.s.str[1] == 2){
+  if (d.s.str[0] == 2 || d.s.str[1] == 2 || d.s.str[2] == 2){
     d.out = true 
     return .3
   }
--- source/msi-group/script.js
+++ source/msi-group/script.js
@@ -100,7 +100,7 @@ var teamSel = treamGroupSel.appendMany(ƒ(), 'g.team-dot')
 teamSel.append('rect')
   .at({width: width, height, fill: ƒ(color)})
 teamSel.st({opacity: d => {
-  if (d.s.str[0] == 2 || d.s.str[1] == 2){
+  if (d.s.str[0] == 2 || d.s.str[1] == 2 || d.s.str[2] == 2){
     d.out = true 
     return .3
   }
