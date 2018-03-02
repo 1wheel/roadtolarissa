30f08f828537e18f36de9503072fa77be2aa7e2e
Adam Pearce <1wheel@gmail.com>
2018-03-02 01:13:29 -0500

    same sex marriage

---
+++ source/_posts/2018-03-01-same-sex-legal.md
@@ -0,0 +1,27 @@
+---
+template: post.html
+title: The Rise and Fall of Same Sex Marriage Bans 
+permalink: /same-sex-legal
+shareimg: https://i.imgur.com/YeoOlAC.png
+draft: true
+---
+
+<div class='chart'></div>
+
+After [Baehr v. Miike](https://en.wikipedia.org/wiki/Baehr_v._Miike) raised the possibly of courts requiring states to grant same sex marriage licenses, state legislatures stated passing laws banning same sex marriage and recognition of out of state sames sex marriages. 
+
+To prevent theses laws from being overturned, conservatives campaigned to amended state constitutions to explicitly ban same sex marriage. In November 2004, 11 states had one of the ballot measures up for a vote. All of them [passed](http://www.nbcnews.com/id/6383353/ns/politics/t/voters-pass-all-bans-gay-marriage/). 
+
+[Obergefell v. Hodges](https://en.wikipedia.org/wiki/Obergefell_v._Hodges) legalized same sex marriage across the country in 2015.
+
+Made for [Reddit's February Visualization Challenge](https://www.reddit.com/r/dataisbeautiful/comments/7vegvf/battle_dataviz_battle_for_the_month_of_february/). Be sure to also check out [Bloomberg's Pace of Social Change](https://www.bloomberg.com/graphics/2015-pace-of-social-change/). 
+
+
+[data](http://www.pewforum.org/2015/06/26/same-sex-marriage-state-by-state/) // [code](https://github.com/1wheel/roadtolarissa/blob/master/source/same-sex-legal/script.js).
+
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src='../worlds-group-2017/d3_.js'></script>
+<script src='../worlds-group-2017/swoopy-drag.js'></script>
+<script src='script.js'></script>
---
+++ source/same-sex-legal/script.js
@@ -0,0 +1,171 @@
+console.clear()
+
+var tidy = []
+
+function fill(d){
+  var s = d.status
+  return s == 'Legal' ? 'rgb(33, 150, 243)' : 
+    s == 'No Law' ? '#bbb' : 
+    s == 'Statutory Ban' ? 'rgb(255, 152, 0)' : 'rgb(244, 67, 54)'
+}
+
+function statusVal(d){
+  var s = d.status
+  return s == 'Legal' ? 1 : 
+    s == 'No Law' ? 0 : 
+    s == 'Statutory Ban' ? -1 : -3
+
+}
+
+
+d3.loadData('data.csv', (err, res) => { 
+  data = res[0]
+
+  var years = data.columns.filter(d => d != 'State' && d != 'abbrev')
+
+  data.forEach(d => {
+    var abv = d.abbrev
+    years.forEach(year => {
+      tidy.push({year, abv, status: d[year]})
+    })
+  })
+
+  var c = d3.conventions({
+    sel: d3.select('.chart').html(''),
+    height: 800,
+    width: 400,
+    margin: {left: 0, top: 350, bottom: 400}
+  })
+
+  c.x.domain([1995, 2015])
+  c.y.domain([0, 50])
+
+  c.xAxis.tickFormat(d => d).tickValues([1995, 2000, 2005, 2010, 2015])
+  d3.drawAxis(c)
+
+  c.svg.select('.x')
+    .translate([7, -c.margin.top - 25])
+    .selectAll('line')
+    .translate([0, 25])
+
+  byState = d3.nestBy(tidy, d => d.abv)
+  byState.forEach(state => {
+    state.forEach((d, i) => {
+      if (!i) return
+      if (d.status != state[i - 1].status) d.changed = true
+    })
+  })
+
+  byYear = d3.nestBy(tidy, d => d.year)
+  byYear.forEach(year => {
+    year.sort(d3.descendingKey(d => statusVal(d)*(d.changed ? .5 : 1)))
+
+    year.forEach((d, i) => d.i = i)
+
+    year.yOffset = d3.sum(year, d => Math.sign(statusVal(d))) - 50
+    year.yOffset = Math.round(year.yOffset/2)*2
+  })
+
+  var yearSel = c.svg.appendMany('g', byYear)
+    .translate(d => [c.x(d.key), c.y(-d.yOffset)/2])
+
+  var circleSel = yearSel.append('circle')
+    .at({fill: 'none', r: 13, strokeWidth: 2})
+    .translate([7, -5])
+
+  var textSel = yearSel.appendMany('text', d => d)
+    .text(d => d.abv)
+    .at({
+      y: d => c.y(d.i),
+      fill,
+      fontSize: 14,
+      fontWeight: d => d.changed ? 900 : 300,
+      opacity: d => d.changed ? 1 : .45
+    })
+    .on('mouseover', d => {
+      textSel
+        .classed('active', 0)
+        .filter(e => d.abv == e.abv)
+        .classed('active', 1)
+
+      circleSel.data(tidy.filter(e => d.abv == e.abv))
+        .at({
+          stroke: fill,
+          cy: d => c.y(d.i),
+          // r: 0
+        })
+    })
+
+  annotations = 
+[
+  {
+    "path": "M 336,-320 A 27.75 27.75 0 0 0 304.9999694824219,-298",
+    "text": "Constitutional Ban",
+    "textOffset": [
+      344,
+      -314
+    ],
+    "status": "Constitutional Ban"
+  },
+  {
+    "path": "M -32,-262 A 47.74 47.74 0 0 0 -5,-204",
+    "text": "Statutory Ban",
+    "textOffset": [
+      -30,
+      -282
+    ],
+    "status": "Statutory Ban"
+  },
+  {
+    "path": "M 144,564 A 45.228 45.228 0 0 0 109,512",
+    "text": "No Law",
+    "textOffset": [
+      97,
+      575
+    ],
+    "status": "No Law"
+  },
+  {
+    "path": "M 239,559 A 38.382 38.382 0 0 1 256,501",
+    "text": "Legal",
+    "textOffset": [
+      244,
+      570
+    ],
+    "status": "Legal"
+  }
+]
+annotations.forEach(d => d.status = d.text)
+
+
+  var swoopy = d3.swoopyDrag()
+      .draggable(0)
+      .x(d => 0)
+      .y(d => 0)
+      .annotations(annotations)
+
+  swoopySel = c.svg.append('g.annotations').call(swoopy)
+  swoopySel.selectAll('path').attr('marker-end', 'url(#arrow)')
+
+  swoopySel.selectAll('text')
+    .st({fill, fontWeight: 600})
+    .each(function(d){
+      d3.select(this)
+          .text('')                        
+          .tspans(d3.wordwrap(d.text,10)) 
+    })  
+
+
+
+})
+
+
+d3.select('html').selectAppend('svg.marker')
+  .append('marker')
+    .attr('id', 'arrow')
+    .attr('viewBox', '-10 -10 20 20')
+    .attr('markerWidth', 20)
+    .attr('markerHeight', 20)
+    .attr('orient', 'auto')
+  .append('path')
+    .attr('d', 'M-6.75,-6.75 L 0,0 L -6.75,6.75')
